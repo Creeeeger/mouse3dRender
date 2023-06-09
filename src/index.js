d0a6e03e888af01550268b53830bb28ee2f2171e
Creeeeger <104427569+Creeeeger@users.noreply.github.com>
2023-06-09 13:37:59 +0100

    feat: add HDR environment lighting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 
 const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
@@ -10,6 +11,9 @@ const renderer = new THREE.WebGLRenderer({
   alpha: true
 });
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 1;
+renderer.outputColorSpace = THREE.SRGBColorSpace;
 
 const camera = new THREE.PerspectiveCamera(
   45,
@@ -26,6 +30,25 @@ controls.minDistance = 1.5;
 controls.maxDistance = 10;
 controls.target.set(0, 0.75, 0);
 controls.update();
+
+const pmremGenerator = new THREE.PMREMGenerator(renderer);
+pmremGenerator.compileEquirectangularShader();
+
+new RGBELoader()
+  .setDataType(THREE.HalfFloatType)
+  .load(
+    'src/royal_esplanade_1k.hdr',
+    (texture) => {
+      const envMap = pmremGenerator.fromEquirectangular(texture).texture;
+      scene.background = envMap;
+      scene.environment = envMap;
+
+      texture.dispose();
+      pmremGenerator.dispose();
+    },
+    undefined,
+    (error) => console.error('Failed to load HDR environment.', error)
+  );
 
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight;
